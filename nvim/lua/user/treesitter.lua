7374573ec1bdc86f4eb0513edc2a98d8a14ad24d
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2022-09-05 23:37:35 +0200

    nvim: use virtual text for lsp

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -17,7 +17,8 @@ configs.setup {
     "scss",
     "typescript",
     "yaml",
-    "nix"
+    "nix",
+    "go"
   }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
