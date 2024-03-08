c2f5c2a4e463389117d25eeaeaee274806695fb3
Raagh <pattferraggi@gmail.com>
2024-03-08 16:59:47 +0100

    nvim: remove incorrect highlights

--- shared/nvim/lua/plugins/editor.lua
+++ shared/nvim/lua/plugins/editor.lua
@@ -77,7 +77,7 @@ return {
         {
           elements = {
             "console",
-            "repl",
+            -- "repl",
           },
           size = 0.30,
           position = "bottom",
@@ -93,10 +93,6 @@ return {
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open({})
       end
-
-      vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#f7768e", bg = "#1a1b26" })
-      vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#449dab", bg = "#1a1b26" })
-      vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#e0af68", bg = "#1a1b26" })
     end,
   },
   {
