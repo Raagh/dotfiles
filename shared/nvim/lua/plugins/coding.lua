573475a00b04f3d0a72b3b9cbffc5912cf8c8220
Raagh <pattferraggi@gmail.com>
2024-03-09 16:17:53 +0100

    nvim: add eslint and prettier extras

--- shared/nvim/lua/plugins/coding.lua
+++ shared/nvim/lua/plugins/coding.lua
@@ -2,7 +2,24 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      opts.servers.tsserver.root_dir = require("lspconfig.util").root_pattern(".git")
+      opts.servers = {
+        eslint = {},
+        tsserver = {
+          root_dir = require("lspconfig.util").root_pattern(".git"),
+        },
+      }
+      opts.setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      }
+
       require("lspconfig.ui.windows").default_options.border = "rounded"
     end,
   },
