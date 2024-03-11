671ddd8c6bcabc18b40cf5c5919a620ac7487a2b
Raagh <pattferraggi@gmail.com>
2024-03-11 22:47:35 +0100

    nvim: reorder config

--- shared/nvim/lua/plugins/coding.lua
+++ shared/nvim/lua/plugins/coding.lua
@@ -61,4 +61,54 @@ return {
       end
     end,
   },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "haydenmeade/neotest-jest",
+    },
+    opts = {
+      discovery = {
+        enabled = false,
+      },
+      quickfix = {
+        enabled = true,
+        open = false,
+      },
+      adapters = {
+        ["neotest-jest"] = {
+          jestCommand = "yarn test --",
+          jestConfigFile = function()
+            local file = vim.fn.expand("%:p")
+            if string.find(file, "/packages/") then
+              return string.match(file, "(.-/[^/]+/)src") .. "jest.config.js"
+            end
+
+            if string.find(file, "/services/") then
+              return string.match(file, "(.-/[^/]+/)src") .. "jest.config.js"
+            end
+
+            return vim.fn.getcwd() .. "/jest.config.js"
+          end,
+          cwd = function()
+            local file = vim.fn.expand("%:p")
+
+            if string.find(file, "/packages/") then
+              return string.match(file, "(.-/[^/]+/)src")
+            end
+
+            if string.find(file, "/services/") then
+              return string.match(file, "(.-/[^/]+/)src")
+            end
+
+            return vim.fn.getcwd()
+          end,
+        },
+      },
+      summary = {
+        open = "topleft vsplit | vertical resize 50",
+      },
+    },
+  },
 }
