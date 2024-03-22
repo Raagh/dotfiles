4bfab208a9030dce62186329e6d2a6f582d35cd9
Raagh <pattferraggi@gmail.com>
2024-03-22 18:37:15 +0100

    nvim: add neotest removed by mistake and update plugins

--- shared/nvim/lua/plugins/coding.lua
+++ shared/nvim/lua/plugins/coding.lua
@@ -64,7 +64,9 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
-      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "haydenmeade/neotest-jest",
     },
     opts = {
       discovery = {
