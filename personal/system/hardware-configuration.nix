c7cf2bd7b866f9f3e74fbb8f7b630f7f96253808
Raagh <pattferraggi@gmail.com>
2024-04-10 22:16:51 +0200

    personal: new nixos basic config

--- personal/system/hardware-configuration.nix
+++ personal/system/hardware-configuration.nix
@@ -14,23 +14,29 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c7aab81d-f086-49c7-aafa-4b811c22c159";
+    { device = "/dev/disk/by-uuid/0d5843c5-5fd6-4837-a972-78f7dc48f250";
       fsType = "ext4";
     };
 
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/CBD6-94B1";
+  boot.initrd.luks.devices."luks-fc4486ae-e675-4b66-b7c8-ea831ce3bdae".device = "/dev/disk/by-uuid/fc4486ae-e675-4b66-b7c8-ea831ce3bdae";
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/2516-A0EC";
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/86b22845-8da2-480c-9acd-21c92bf3b2db"; }
+    ];
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = lib.mkDefault false;
-  networking.interfaces.wlp114s0.useDHCP = lib.mkDefault true;
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s13f0u3u4.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp114s0.useDHCP = lib.mkDefault true;
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
