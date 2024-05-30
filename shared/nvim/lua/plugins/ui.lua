dfb4418a0b5d5981406256bc9a17e3060fa41e0e
Patricio Ferraggi Ares <pferraggi@Patricios-MacBook-Pro.local>
2024-05-30 15:55:47 +0200

    nvim: update plugins and fix bufferline keybindings

--- shared/nvim/lua/plugins/ui.lua
+++ shared/nvim/lua/plugins/ui.lua
@@ -21,14 +21,14 @@ return {
     "akinsho/bufferline.nvim",
     keys = {
       {
-        "[[",
+        "<leader>b<",
         function()
           require("bufferline").move(-1)
         end,
         desc = "Move Buffer Left",
       },
       {
-        "]]",
+        "<leader>b>",
         function()
           require("bufferline").move(1)
         end,
