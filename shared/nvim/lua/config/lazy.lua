af8847a963318592612c95284db9a6d6e1ff17c1
Raagh <pattferraggi@gmail.com>
2024-06-11 20:14:15 +0200

    nvim: cleanup mandatory packages

--- shared/nvim/lua/config/lazy.lua
+++ shared/nvim/lua/config/lazy.lua
@@ -12,8 +12,6 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.nix" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
