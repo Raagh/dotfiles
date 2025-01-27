3ff1991217656974ab54dd3a52b493e1a05df025
Raagh <pattferraggi@gmail.com>
2025-01-27 23:25:58 +0100

    personal: lets burn everything to the ground there is no way back

--- personal/home-manager/rofi.nix
+++ personal/home-manager/rofi.nix
@@ -5,18 +5,18 @@
 }:
 
 {
-  # programs.rofi = {
-  #   enable = true;
-  # };
-
-  # Packages to make hyprland a DE.
-  home.packages = with pkgs; [
-    rofi-wayland
-  ];
-
-  # link the hyprland config to the .config folder in home
-  home.file = {
-    ".config/rofi/".source =
-      config.lib.file.mkOutOfStoreSymlink "/home/raagh/Code/dotfiles/personal/config/rofi/";
+  programs.rofi = {
+    enable = true;
   };
+
+  # # Packages to make hyprland a DE.
+  # home.packages = with pkgs; [
+  #   rofi-wayland
+  # ];
+  #
+  # # link the hyprland config to the .config folder in home
+  # home.file = {
+  #   ".config/rofi/".source =
+  #     config.lib.file.mkOutOfStoreSymlink "/home/raagh/Code/dotfiles/personal/config/rofi/";
+  # };
 }
