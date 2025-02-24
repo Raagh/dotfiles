47d3f846b6f64fbb8449dd5988ced6671eae4a4d
Raagh <pattferraggi@gmail.com>
2025-02-24 19:32:49 +0100

    personal: move cursor to nix config so it applies to gdm

--- personal/home-manager/theme.nix
+++ personal/home-manager/theme.nix
@@ -18,12 +18,6 @@
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
 
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-      size = 24;
-    };
-
     fonts = {
       serif = {
         package = pkgs.dejavu_fonts;
--- personal/nixos/theme.nix
+++ personal/nixos/theme.nix
@@ -4,10 +4,17 @@
   stylix = {
 
     # Here I set it so grub and gdm follow that theme too.
+
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
     #FIXME: this propery is mandatory in the current version
     # but in future version I will only set this on the home configuration.
     image = ../../assets/Shogoki.png;
+
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
   };
 }
