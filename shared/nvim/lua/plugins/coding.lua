7866bc8eff410de5019ccc799770bbb3b2aa509f
Raagh <pattferraggi@gmail.com>
2025-04-07 10:17:14 -0300

    nvim: workaround for lualine and copilot

--- shared/nvim/lua/plugins/coding.lua
+++ shared/nvim/lua/plugins/coding.lua
@@ -1,4 +1,17 @@
 return {
+  ---FIXME: Temporary hack till lualine gets fixed
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections.lualine_x[2] = LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
+        local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+        if #clients > 0 then
+          local status = require("copilot.status").data.status
+          return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+        end
+      end)
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
