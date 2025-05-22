7879147a75deab2d7c9356fedf822f02b1358ba6
Raagh <pattferraggi@gmail.com>
2025-05-22 14:03:49 +0200

    corne: going back to markstos base, not giving up

--- shared/corne/qmk/raagh/config.h
+++ shared/corne/qmk/raagh/config.h
@@ -2,15 +2,30 @@
 
 #define SPLIT_USB_DETECT
 
-/* Select hand configuration */
+// Select hand configuration
 #define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
+
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM_THUMB 250
+
+// If you press a dual-role key, press another key, and then release the
+// dual-role key, all within the tapping term, by default the dual-role key
+// will perform its tap action. If the HOLD_ON_OTHER_KEY_PRESS option is
+// enabled, the dual-role key will perform its hold action instead.
+// #define HOLD_ON_OTHER_KEY_PRESS
+
+// When enabled, typing a mod-tap plus second within term will register as the mod-combo
+// Ref: https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold 
+// #define PERMISSIVE_HOLD
+
+// Prevent keydown and keyup from firing on different layers
+#define PREVENT_STUCK_MODIFERS
 
 // Set the COMBO_TERM so low that I won't type the keys one after each other during normal typing.
 // They would have be held together intentionally to trigger this.
 #define COMBO_TERM 40
-#define COMBO_COUNT 2
+#define COMBO_COUNT 3
 
 // These mostly affect my one-shot Shift key, providing a CapsLock alternative.
 // I want a relatively low timeout, so if I accidentally type "Shift", I can pause just briefly and move on.
--- shared/corne/qmk/raagh/keymap.c
+++ shared/corne/qmk/raagh/keymap.c
@@ -1,5 +1,17 @@
 #include QMK_KEYBOARD_H
 
+extern uint8_t is_master;
+
+// markstos defines
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  FUNC,
+  BACKLIT
+};
+
 enum combos {
   DF_TAB,
   JK_ESC
@@ -15,24 +27,38 @@ combo_t key_combos[COMBO_COUNT] = {
   [JK_ESC]    = COMBO(jk_combo, KC_ESC),
 };
 
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _FUNC 3
+
+// For _QWERTY layer
 #define OSM_LCTL OSM(MOD_LCTL)
-#define OSM_AGR  OSM(MOD_RALT)
+#define OSM_LALT OSM(MOD_LALT)
+// #define OSM_AGR  OSM(MOD_RALT)
+#define OSL_FUN  OSL(_FUNC)
+// #define GUI_ENT  GUI_T(KC_ENT)
+#define LOW_SPC  LT(_LOWER, KC_SPC)
+#define RSE_BSP  LT(_RAISE, KC_BSPC)
 #define OSM_SFT  OSM(MOD_LSFT)
 
+// For _RAISE layer
+#define CTL_ESC  LCTL_T(KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_split_3x6_3(
+  [_QWERTY] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , OSL(3) ,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     OSM_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                      KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+     OSM_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     OSM_SFT , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                      KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, OSM_AGR,
+ OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,OSL_FUN,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          OSL(1) , KC_LGUI, KC_SPC ,    KC_BSPC, KC_ENT , OSL(2)
+                                         OSM_LALT,  KC_RGUI, LOW_SPC,   RSE_BSP ,KC_ENT  ,OSM_SFT \
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [1] = LAYOUT_split_3x6_3(
+  [_LOWER] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -40,24 +66,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX, KC_TILD, KC_GRV , KC_LBRC, KC_LCBR,                      KC_RCBR, KC_RBRC, KC_COMM,  KC_DOT, KC_SLSH, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                          KC_TRNS,  KC_TRNS,XXXXXXX,    RSE_BSP, KC_TRNS, KC_COLON \
                                       //`--------------------------'  `--------------------------'
-  ),
+    ),
 
-  [2] = LAYOUT_split_3x6_3(
+
+  [_RAISE] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, KC_DEL , XXXXXXX, KC_UNDS, KC_PLUS, KC_PGUP,                      XXXXXXX, KC_PIPE, KC_BSLS, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_HOME, KC_END , KC_MINS, KC_EQL , KC_PGDN,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, KC_LT  , KC_GT  , XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+      _______, XXXXXXX, XXXXXXX, KC_LT  , KC_GT  , KC_SCLN,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______ 
+                                          CTL_ESC, KC_TRNS, LOW_SPC,    XXXXXXX , KC_TRNS, KC_TRNS\
                                       //`--------------------------'  `--------------------------'
   ),
 
-
-  [3] = LAYOUT_split_3x6_3(
+  [_FUNC] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_F1  , KC_F2  , KC_F3   , KC_F4 ,  KC_F5 , KC_F6  ,                      KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -65,8 +91,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_VOLD, KC_BRID, XXXXXXX, XXXXXXX, QK_BOOT,                     RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX\
                                       //`--------------------------'  `--------------------------'
   )
 };
 
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+      case LT(_RAISE, KC_BSPC):
+            return TAPPING_TERM_THUMB;
+      case LT(_LOWER, KC_SPC):
+            return TAPPING_TERM_THUMB;
+      default:
+            return TAPPING_TERM;
+    }
+}
+
+// bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//       case LT(_RAISE, KC_BSPC):
+//             return false;
+//       case LT(_LOWER, KC_SPC):
+//             return false;
+//         default:
+//             return true;
+//     }
+// }
