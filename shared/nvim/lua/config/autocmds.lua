f5799b695607d33ff739b1323909d631e981419e
Raagh <pattferraggi@gmail.com>
2025-09-23 11:56:41 +0200

    nvim: fix lsp bubble and markdown

--- shared/nvim/lua/config/autocmds.lua
+++ shared/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(event)
+    vim.schedule(function()
+      require("noice.text.markdown").keys(event.buf)
+    end)
+  end,
+})
--- shared/nvim/lua/plugins/coding.lua
+++ shared/nvim/lua/plugins/coding.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = function(_, _)
+      require("lspconfig.ui.windows").default_options.border = "rounded"
+      vim.diagnostic.config({
+        float = { border = "rounded" },
+      })
+    end,
+  },
+  {
     "folke/snacks.nvim",
     keys = {
       {
