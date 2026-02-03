2e45a412830d69c44b3ad7cb291871ca531adfe1
Raagh <pattferraggi@gmail.com>
2026-02-03 18:51:15 +0100

    shared: remove old corne and save new layout

--- shared/corne/qmk/raagh/rules.mk
+++
@@ -1,8 +0,0 @@
-BOOTLOADER                  = caterina
-VIA_ENABLE                  = yes
-RGBLIGHT_ENABLE             = yes
-MOUSEKEY_ENABLE             = no
-EXTRAKEY_ENABLE             = yes # enable media keys
-LTO_ENABLE                  = yes # smaller file size, little down-side
-COMBO_ENABLE                = yes
-DYNAMIC_TAPPING_TERM_ENABLE = yes
--- shared/corne/special-keys.md
+++
@@ -1,3 +0,0 @@
-0x7c70 dynamic tapping term print
-0x7c71 dynamic tapping term up
-0x7c72 dynamic tapping term down
